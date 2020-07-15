9fa0b77f49a323057e9e719ec0379c49b6f49ac7
Jim Urbas <git@jimu.net>
2020-07-15 02:57:04 -0500

    [Lesson 1.4] Add heading and speed control
    Includes all content from lesson 1.4
    
    Input controls speed and heading of player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,17 @@ public class PlayerController : MonoBehaviour
 {
 
     public float speed = 0.25f;
+    public float accelleration = 5f;
+    public float turnSpeed = 30f;
     void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        speed += verticalInput * Time.deltaTime * accelleration;
+
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * turnSpeed);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
