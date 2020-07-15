82e5ff42bc085c676b0a5e8d1b844d3ca40a13f7
Jim Urbas <git@jimu.net>
2020-07-15 01:42:29 -0500

    [Lesson 1.3] Camera follows player
    Includes all work upto and including Lesson 1.3.
    
    The camera is translated to player position, is offset by a public
    vector amount, and is set to look at the player

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset;
+    
+    private void Awake()
+    {
+        
+    }
+    void LateUpdate()
+    {
+        transform.rotation = player.transform.rotation;
+        transform.position = player.transform.position;
+        transform.Translate(offset);
+        transform.LookAt(player.transform);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 0.25f;
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
